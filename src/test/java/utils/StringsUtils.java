0aa923212fbcd064fa60a0654bad26b927201564
cristianciula <cristianciula@gmail.com>
2024-01-14 17:23:36 +0200

    Renaming improvements

--- src/test/java/utils/StringsUtils.java
+++ src/test/java/utils/StringsUtils.java
@@ -7,10 +7,7 @@ public final class StringsUtils {
         // Remove the square brackets and double quotes
         String cleanedInput = input.replaceAll("[\\[\\]\"]", "");
 
-        // Split the cleaned string into an array using the comma as a delimiter
-        String[] array = cleanedInput.split(",");
-
-        return array;
+        // Return the cleaned string as an array by adding the comma as a delimiter
+        return cleanedInput.split(",");
     }
-
 }
