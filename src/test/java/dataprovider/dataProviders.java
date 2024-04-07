a5a784dfd33092c31a3cdfb5e72b0dadb3a431ab
cristianciula <cristianciula@gmail.com>
2024-04-07 21:22:59 +0300

    Added a TODO in LoginTests

--- src/test/java/dataprovider/dataProviders.java
+++ src/test/java/dataprovider/dataProviders.java
@@ -17,18 +17,30 @@ public class dataProviders {
         return new Object[][] {
 
             // Invalid Email with Valid Password
-                {"", "Parola100!"},
-                {" ", "Parola100!"},
                 {"aexample.com", "Parola100!"},
                 {"a@b.c", "Parola100!"},
                 {"_@_._", "Parola100!"},
                 {"unregisteredemail@example.com", "Parola100!"},
 
             //Valid Email with Invalid Password
-                {"testuser01@example.com", ""},
-                {"testuser137@example.com", "Parola12345!"},
-                {"testuser01@example.com", " "},
                 {"testuser01@example.com", "invalidPassword"}
+        };
+    }
+
+    @DataProvider (name = "emptyCredentials")
+    public static Object[][] emptyCredentials() {
+        return new Object[][] {
+
+                // Empty Email and Empty Password
+                {"", ""},
+                {" ", " "},
+
+                // Empty Email and Valid Password
+                {"", "Parola12345!"},
+
+                // Valid Email and Empty Password
+                {"testuser137@example.com", ""},
+                {"testuser137@example.com", "Parola12345!"}, //test the test
         };
     }
 
