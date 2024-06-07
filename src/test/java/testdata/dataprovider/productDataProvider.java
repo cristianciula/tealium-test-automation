716ebf07283f33c0e85be4815640b6964be511ee
cristianciula <cristianciula@gmail.com>
2024-06-07 11:12:19 +0300

    DataProviderTemplate

--- src/test/java/testdata/dataprovider/productDataProvider.java
+++ src/test/java/testdata/dataprovider/productDataProvider.java
@@ -1,6 +1,10 @@
 package testdata.dataprovider;
 
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static testdata.dataprovider.userDataProvider.validLoginCredentials;
 
@@ -15,4 +19,25 @@ public class productDataProvider {
                 {validEmail, validPassword}
         };
     }
+
+    @DataProvider
+    public Object[][] combinedDataTemplate() {
+
+        //Prerequisites
+        Object[][] dataProvider1 = dataProvider1();
+        Object[][] dataProvider2 = dataProvider2();
+        List<Object[]> combinedDataProviders = new ArrayList<>();
+
+        //Combine the data
+        for (Object[] dataProvider1Obj : dataProvider1) {
+            for (Object[] dataProvider2Obj : dataProvider2) {
+                Object[] combinedItem = new Object[dataProvider1Obj.length + dataProvider2Obj.length];
+                System.arraycopy(dataProvider1Obj, 0, combinedItem, 0, dataProvider1Obj.length);
+                System.arraycopy(dataProvider2Obj, 0, combinedItem, dataProvider1Obj.length, dataProvider2Obj.length);
+                combinedDataProviders.add(combinedItem);
+            }
+        }
+        return combinedDataProviders.toArray(new Object[combinedDataProviders.size()][]);
+    }
+
 }
